a5ce59290a85c3278d5341f50ff04a006bac1c47
Isaac Garcia <joelgarcia405@gmail.com>
2024-03-23 19:52:00 -0600

    termina ssr list y ssr client

--- src/routes/pokemons/list-client/index.tsx
+++ src/routes/pokemons/list-client/index.tsx
@@ -1,4 +1,4 @@
-import { component$, useStore, useVisibleTask$ } from '@builder.io/qwik';
+import { $, component$, useOnDocument, useStore, useTask$, useVisibleTask$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
 
 import { PokemonImage } from '~/components/pokemons/pokemon-image';
@@ -7,6 +7,7 @@ import type { SmallPokemon } from '~/interfaces';
 
 interface PokemonPageState {
     currentPage: number;
+    isLoading: boolean;
     pokemons: SmallPokemon[];
 }
 
@@ -15,13 +16,41 @@ export default component$(() => {
     
     const pokemonState = useStore<PokemonPageState>({
         currentPage: 0,
+        isLoading: false,
         pokemons: []
     });
 
-    useVisibleTask$(async() => {
-        const pokemons = await getSmallPokemons( pokemonState.currentPage * 10);
-        pokemonState.pokemons = pokemons;
-    })
+    // useVisibleTask$(async({ track }) => {
+
+    //     track( () => pokemonState.currentPage);
+
+    //     const pokemons = await getSmallPokemons( pokemonState.currentPage * 10);
+    //     pokemonState.pokemons = [ ...pokemonState.pokemons, ...pokemons ];
+    // });
+
+    
+    useTask$(async({ track }) => {
+
+        track( () => pokemonState.currentPage);
+
+        pokemonState.isLoading = true;
+
+        const pokemons = await getSmallPokemons( pokemonState.currentPage * 10, 30);
+        pokemonState.pokemons = [ ...pokemonState.pokemons, ...pokemons ];
+
+        pokemonState.isLoading = false;
+    });
+
+    useOnDocument('scroll', $((event) => {
+        const maxScroll = document.body.scrollHeight;
+        const currentScroll = window.scrollY + window.innerHeight;
+
+        if ( (currentScroll + 200) >= maxScroll && !pokemonState.isLoading){
+            pokemonState.isLoading = true;
+            pokemonState.currentPage++;
+        }
+
+    }))
 
     return (
         <>
@@ -32,11 +61,11 @@ export default component$(() => {
             </div>
 
             <div class="mt-10">
-                <button onClick$={() => pokemonState.currentPage-- } class="btn btn-primary mr-2">Anteriores</button>
+                {/* <button onClick$={() => pokemonState.currentPage-- } class="btn btn-primary mr-2">Anteriores</button> */}
                 <button onClick$={() => pokemonState.currentPage++ }class="btn btn-primary mr-2">Siguientes</button>
             </div>
 
-            <div class="grid grid-cols-6 mt-5">
+            <div class="grid sm:grid-cols-2 md:grid-cols-5 xl:grid-cols-7 grid-cols-6 mt-5">
                 {
                     pokemonState.pokemons.map(({name, id}) => (
                         <div key={ name } class="m-5 flex flex-col justify-center items-center">
